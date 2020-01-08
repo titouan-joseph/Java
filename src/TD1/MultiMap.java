8920d4c6a4ccff8640cfbb2d4ecfd8ff51e8781d
Titouan-Joseph <titouanjoseph@gmail.com>
2020-01-08 19:17:42 +0100

    finishing MultiMap

--- src/TD1/MultiMap.java
+++ src/TD1/MultiMap.java
@@ -1,11 +1,12 @@
 package TD1;
 
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MultiMap<K, V> implements Map<K, List<V>> {
+public class MultiMap<K, V> extends HashMap<K, List<V>> implements Map<K, List<V>> {
 
     public void putOneValue(K akey, V avalue){
         List<V> rlst;
@@ -18,8 +19,14 @@ public class MultiMap<K, V> implements Map<K, List<V>> {
         put(akey, rlst);
     }
 
-    public boolean containsOneValue(V){
-        for( Map.Entry)
+    public boolean containsOneValue(V aValue){
+        for( Map.Entry<K, List<V>> e : entrySet()){
+            List<V> rlst = e.getValue();
+            if(rlst.contains(aValue)){
+                return true;
+            }
+        }
+        return false;
     }
 
 }
